234cf45666db5550424788a63cc3cf06122a36cf
Brea Ricardo <brea.ricardo@gmail.com>
2019-01-14 23:30:28 -0300

    using route's data to put things on it to later du a automatic menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent ,data: {info: 'Alta ruta ameo'} }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,32 +2,31 @@ import {
   Component, 
   ChangeDetectorRef, 
   EventEmitter, 
-  Output } from '@angular/core';
+  Output, 
+  OnInit} from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { MatSidenav } from '@angular/material';
+import { Router, Route } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit  {
   title = 'Material PWA';
   mobileQuery: MediaQueryList;
   nav = [
     {
       'title': 'Home',
-      'path': '/'
-    },
-    {
-      'title': 'My Account (Part 2)',
-      'path': '/auth'
+      'path': '/',
+      
     }
   ];
   private _mobileQueryListener: () => void;
   @Output() toggleSideNav = new EventEmitter();
   
-  constructor( changeDetectorRef: ChangeDetectorRef, media: MediaMatcher ) {
+  constructor(private router: Router, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
@@ -36,6 +35,19 @@ export class AppComponent {
   toggleMobileNav(nav: MatSidenav) {
     if (this.mobileQuery.matches) {
       nav.toggle();
+    }
+  }
+  ngOnInit() {
+    this.printpath('', this.router.config);
+  }
+  printpath(parent: String, config: Route[]) {
+    for (let i = 0; i < config.length; i++) {
+      let route = config[i];
+      console.log(route.data.info ? route.data.info: 'naranja');
+      if (route.children) {
+        const currentPath = route.path ? parent + '/' + route.path : parent;
+        this.printpath(currentPath, route.children);
+      }
     }
   }
 }
